29896a822c5b06026ac583437a188a3e2facbdfc
Futa119 <uemura.futa0119@gmail.com>
2025-11-15 17:22:28 +0900

    ピンクガーネットブロック追加

--- src/main/java/com/example/ThxKaupenjoe.java
+++ src/main/java/com/example/ThxKaupenjoe.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import com.example.block.ModBlocks;
+import com.example.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -7,18 +9,11 @@ import org.slf4j.LoggerFactory;
 
 public class ThxKaupenjoe implements ModInitializer {
 	public static final String MOD_ID = "thx-kaupenjoe";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/com/example/block/ModBlocks.java
@@ -0,0 +1,40 @@
+package com.example.block;
+
+import com.example.ThxKaupenjoe;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    private static Block registerBlock(String name,Block block){
+        registerBlockItem(name,block);
+        return Registry.register(Registries.BLOCK, Identifier.of(ThxKaupenjoe.MOD_ID,name),block);
+    }
+
+    private static void registerBlockItem(String name, Block block){
+        Registry.register(Registries.ITEM, Identifier.of(ThxKaupenjoe.MOD_ID,name),
+                new BlockItem(block,new Item.Settings()));
+    }
+
+    public static void registerModBlocks(){
+        ThxKaupenjoe.LOGGER.info("reg mod blocks");
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries ->{
+            entries.add(ModBlocks.PINK_GARNET_BLOCK);
+        });
+    }
+
+
+}
