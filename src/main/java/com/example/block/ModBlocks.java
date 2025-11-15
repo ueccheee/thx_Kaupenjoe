390c05800015d1e7cac489649be4c83d9744b469
Futa119 <uemura.futa0119@gmail.com>
2025-11-16 00:49:50 +0900

    生ピンクガーネットブロック追加

--- src/main/java/com/example/block/ModBlocks.java
+++ src/main/java/com/example/block/ModBlocks.java
@@ -18,6 +18,10 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create().strength(4f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
+    public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
     private static Block registerBlock(String name,Block block){
         registerBlockItem(name,block);
         return Registry.register(Registries.BLOCK, Identifier.of(ThxKaupenjoe.MOD_ID,name),block);
@@ -33,6 +37,7 @@ public class ModBlocks {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries ->{
             entries.add(ModBlocks.PINK_GARNET_BLOCK);
+            entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
         });
     }
 
